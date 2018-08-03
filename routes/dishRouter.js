876d704a0852a9b37747bcbb616a9856740c5287
Olga Kozhevnikova <olga@starkflow.co>
2018-08-03 13:44:29 +0530

    Implement express REST APIs for comments with Mongoose

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -21,7 +21,7 @@ dishRouter.route('/')
 .post((req, res, next) => {
   Dishes.create(req.body)
   .then((dish) => {
-    console.log('Dish created ', dish);
+    // console.log('Dish created ', dish);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json(dish);
@@ -75,6 +75,166 @@ dishRouter.route('/:dishId')
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
+
+dishRouter.route('/:dishId/comments')
+.get((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    if (dish != null) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(dish.comments);
+    }
+    else {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+.post((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    if (dish != null) {
+      dish.comments.push(req.body);
+      dish.save()
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish.comments);
+      }, (err) => next(err))
+    }
+    else {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+.put((req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation is not supported on /dishes/' + req.params.dishId + '/comments');
+})
+.delete((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    if (dish != null) {
+      for (let i = (dish.comments.length - 1); i >= 0; i--) {
+        // the way to access subdocument and remove each subdocument (comment) from an array of subdocuments (comments)
+        dish.comments.id(dish.comments[i]._id).remove();
+      }
+      dish.save()
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish.comments);
+      }, (err) => next(err));
+    }
+    else {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+  }, (err) => next(err))
+  .catch((err) => next(err));  
+});
+
+dishRouter.route('/:dishId/comments/:commentId')
+.get((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    // both dish and comments exist
+    if (dish != null && dish.comments.id(req.params.commentId) != null) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(dish.comments.id(req.params.commentId));
+    }
+    // dish doesn't exist
+    else if (dish == null) {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+    // dish exists, but comments do not exist
+    else {
+      err = new Error('Comment ' + req.params.commentId + ' not found.');
+      err.status = 404;
+      return next(err);
+    }    
+  }, (err) => next(err))
+  .catch((err) => next(err));  
+})
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation is not supported on /dishes/' + req.params.dishId + '/comments/' + req.params.commentId);
+})
+.put((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    // both dish and comments exist
+    if (dish != null && dish.comments.id(req.params.commentId) != null) {
+      // a user can change either rating...
+      if (req.body.rating) {
+        dish.comments.id(req.params.commentId).rating = req.body.rating;
+      }
+      // ... or comment
+      if (req.body.comment) {
+        dish.comments.id(req.params.commentId).comment = req.body.comment;
+      }
+      // after updating rating and/or comment, save the dish
+      dish.save()
+      // after saving the dish, send a reply
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+      }, (err) => next(err));
+    }
+    // dish doesn't exist
+    else if (dish == null) {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+    // dish exists, but comments do not exist
+    else {
+      err = new Error('Comment ' + req.params.commentId + ' not found.');
+      err.status = 404;
+      return next(err);
+    }    
+  }, (err) => next(err))
+  .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+  Dishes.findById(req.params.dishId)
+  .then((dish) => {
+    if (dish != null && dish.comments.id(req.params.commentId) != null) {
+      dish.comments.id(req.params.commentId).remove();
+      // save the dish after removing a comment
+      dish.save()
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+      }, (err) => next(err));
+    }
+    // dish doesn't exist
+    else if (dish == null) {
+      err = new Error('Dish ' + req.params.dishId + ' does not exist.');
+      err.status = 404;
+      return next(err);
+    }
+    // dish exists, but comments do not exist
+    else {
+      err = new Error('Comment ' + req.params.commentId + ' not found.');
+      err.status = 404;
+      return next(err);
+    }
   }, (err) => next(err))
   .catch((err) => next(err));
 });
