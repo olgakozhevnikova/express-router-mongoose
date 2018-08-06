bbb31be84b4f284c852431439a900aab4352b7c6
Olga Kozhevnikova <olga@starkflow.co>
2018-08-06 23:12:52 +0530

    Add cookies to the project

--- app.js
+++ app.js
@@ -32,40 +32,60 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// pass the secret key to the cookieParser()
+app.use(cookieParser('12345-67890-09876-54321'));
 
 // Authentication function
 function auth(req, res, next) {
-  console.log(req.header);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  //if the signed cookie doesn't contain the user property,
+  // the a user has to use an authorization header for authorization
+  if(!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
 
-  // if authorization header is null, means that a user didn't provide login and password information
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    // if authorization header is null, means that a user didn't provide login and password information
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    // extract authorization header:
+    // base64 is encoding,
+    // after spliting we get an array, the 2nd element of the array contains base64 encoded string, that we use
+    // convert it to the string and then again split, to separate user name from password by :
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+    // if default username and password match the request
+    if (username === 'admin' && password === 'password') {
+      // include cookie with name 'user' and value 'admin'
+      res.cookie('user', 'admin', {signed: true})
+      // this next() means that the request is passed to the next set of middleware (to below app.use())
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
-
-  // extract authorization header:
-  // base64 is encoding,
-  // after spliting we get an array, the 2nd element of the array contains base64 encoded string, that we use
-  // convert it to the string and then again split, to separate user name from password by :
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-
-  // if default username and password match the request
-  if (username === 'admin' && password === 'password') {
-    // this next() means that the request is passed to the next set of middleware (to below app.use())
-    next();
-  }
+  // if the cookie exists and the user property is defined
   else {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    // if signedCookies contains the correct information,
+    if(req.signedCookies.user === 'admin') {
+      // then allow the request to pass through
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
